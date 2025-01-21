220ffe3775ab3e56ca7f30883ef167b81e3c83cc
Lucia Heredia <68028960+LuciaHeredia@users.noreply.github.com>
2025-01-21 20:54:36 +0200

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -9,7 +9,7 @@ sudo systemctl status apache2
 sudo chown -R $USER:$USER /var/www
 
 ### Fetch public IP using instance metadata
-PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+PUBLIC_IP=$(curl -s curl http://checkip.amazonaws.com)
 
 ### Update index.html file with public IP
 echo "<html><body><h1>My Public IP Address is: $PUBLIC_IP</h1></body></html>" > /var/www/html/index.html
