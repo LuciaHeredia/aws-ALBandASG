ab9064ade3f9a23e13b3351a878e0537a80e67b7
Lucia Heredia <68028960+LuciaHeredia@users.noreply.github.com>
2025-01-21 20:22:28 +0200

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -5,8 +5,11 @@ sudo apt update
 sudo apt install apache2 -y
 sudo systemctl status apache2
 
+### Change Ownership permission
+sudo chown -R $USER:$USER /var/www
+
 ### Fetch public IP using instance metadata
-PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 
 ### Update index.html file with public IP
-sed -i "s/<PUBLIC_IP>/$PUBLIC_IP/g" /var/www/html/index.html
+echo "<html><body><h1>My Public IP Address is: $PUBLIC_IP</h1></body></html>" > /var/www/html/index.html
